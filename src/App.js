0280351c9add6033bd630aba384ca9b364f0bf26
Marie-Lou Le Jan <marielou.lejan@gmail.com>
2023-01-10 17:25:51 +0100

    .env example

--- src/App.js
+++ src/App.js
@@ -13,8 +13,6 @@ import { setAuthToken } from "./services/setAuthToken";
 
 function App() {
 
-  console.log(process.env.REACT_APP_MY_ENVIRONMENT_VARIABLE)
-
   const JWT = useSelector(selectJWT);
   if (JWT) setAuthToken(JWT)
 
